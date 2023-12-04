9f93534620fa527e9be3b1f15cbeb22968e101f7
Aaron <aaron6388@hotmail.com>
2023-12-04 01:59:00 +0000

    Unit test for incorrect parameter data types

--- ASE-Assignment-Tests/CommandParserTests.cs
+++ ASE-Assignment-Tests/CommandParserTests.cs
@@ -1,20 +1,31 @@
-using ASE_assignment;
-using Xunit;
+
 namespace ASE_Assignment_Tests
 {
     public class CommandParserTests
     {
         [Fact]
-        public void ParseLine_NoInput()
+        public void ParseLine_InvalidParameters()
         {
             // Arrange 
             ASE_assignment.CommandParser parse = new();
             string commandLine = "rectangle 200 100";
 
-
-
-            // Assert
+            // Act and assert
             Assert.Throws<InvalidOperationException>(() => parse.ParseLine(commandLine));
+        }
+        [Fact]
+        public void ParseLine_ParseWhitespace()
+        {
+            // Arrange
+            ASE_assignment.CommandParser parser = new();
+            string commandLine = "moveto ten,ten";
+
+            // Act
+
+           // var result = parser.ParseLine(commandLine);
+
+            // Act and assert
+            Assert.Throws<FormatException>(() => parser.ParseLine(commandLine));
         }
     }
 }
