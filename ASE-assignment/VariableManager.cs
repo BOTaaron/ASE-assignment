ee053a6fffed82bd142fd5ebd0b0c9be08b6c315
Aaron <aaron6388@hotmail.com>
2024-04-05 03:25:11 +0100

    created variable manager to handle variable creation logic

---
+++ ASE-assignment/VariableManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASE_assignment
+{
+    internal class VariableManager
+    {
+        private Dictionary<string, object> variables = new Dictionary<string, object>();
+        private DataTable dataTable = new DataTable();
+
+        public void DeclareVariable(string stringParameter)
+        {
+
+        }
+        private void EvaluateExpression(string expression)
+        {
+            
+        }
+        public object GetVariable(string name)
+        {
+            if (variables.TryGetValue(name, out object value))
+            {
+                return value;
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Variable '{name}' is not declared");
+            }
+        }
+    }
+}
